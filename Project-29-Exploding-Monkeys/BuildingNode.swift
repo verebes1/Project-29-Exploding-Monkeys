051fc0bf30ccf3f0c0ba5a50a924a7eb383a488e
David Verbenyi <dverbenyi@gmail.com>
2019-02-09 18:20:37 +0000

    Basic setup, clean up.

---
+++ Project-29-Exploding-Monkeys/BuildingNode.swift
@@ -0,0 +1,13 @@
+//
+//  BuildingNode.swift
+//  Project-29-Exploding-Monkeys
+//
+//  Created by verebes on 09/02/2019.
+//  Copyright © 2019 A&D Progress. All rights reserved.
+//
+
+import SpriteKit
+
+class BuildingNode: SKSpriteNode {
+
+}
