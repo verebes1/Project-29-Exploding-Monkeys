ee20723c1100acaa707dd4bf5de6a9040ecf89a9
David Verbenyi <dverbenyi@gmail.com>
2019-02-12 13:15:06 +0000

    Finish playable game. May add menu and sounds in the future.

--- Project-29-Exploding-Monkeys/BuildingNode.swift
+++ Project-29-Exploding-Monkeys/BuildingNode.swift
@@ -11,22 +11,22 @@ import GameplayKit
 
 class BuildingNode: SKSpriteNode {
     
-    var currentImega: UIImage!
+    var currentImage: UIImage!
     
     func setup() {
         name = "building"
         
-        currentImega = drawBuilding(size: size)
-        texture = SKTexture(image: currentImega)
+        currentImage = drawBuilding(size: size)
+        texture = SKTexture(image: currentImage)
         
         configurePhysics()
     }
     
     private func configurePhysics() {
         physicsBody = SKPhysicsBody(texture: texture!, size: size)
-        physicsBody!.isDynamic = false
-        physicsBody!.categoryBitMask = CollisionTypes.building.rawValue
-        physicsBody!.categoryBitMask = CollisionTypes.banana.rawValue
+        physicsBody?.isDynamic = false
+        physicsBody?.categoryBitMask = CollisionTypes.building.rawValue
+        physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
     }
     
     private func drawBuilding(size: CGSize) -> UIImage {
@@ -67,5 +67,29 @@ class BuildingNode: SKSpriteNode {
         }
         //4-Return the result as an UIImage to use elsewhere
         return img
+    }
+    
+    func hitAt(point: CGPoint) {
+//        1. Figure out where the building was hit. Remember: SpriteKit's positions things from the center and Core Graphics from the bottom left!
+        let convertedPoint = CGPoint(x: point.x + size.width / 2.0, y: abs(point.y - (size.height / 2.0)))
+//        2. Create a new Core Graphics context the size of our current sprite.
+        let renderer = UIGraphicsImageRenderer(size: size)
+//        3. Draw our current building image into the context. This will be the full building to begin with, but it will change when hit.
+        let img = renderer.image { ctx in
+            currentImage.draw(at: CGPoint(x: 0, y: 0))
+            
+//        4. Create an ellipse at the collision point. The exact co-ordinates will be 16 points up and to the left of the collision, then 32x32 in size - an ellipse centered on the impact point.
+            ctx.cgContext.addEllipse(in: CGRect(x: convertedPoint.x - 16, y: convertedPoint.y - 16, width: 32, height: 32))
+//        5. Set the blend mode .clear then draw the ellipse, literally cutting an ellipse out of our image.
+            ctx.cgContext.setBlendMode(.clear)
+            ctx.cgContext.drawPath(using: .fill)
+        }
+
+//        6. Convert the contents of the Core Graphics context back to a UIImage, which is saved in the currentImage property for next time we're hit, and used to update our building texture.
+        texture = SKTexture(image: img)
+        currentImage = img
+        
+//        7. Call configurePhysics() again so that SpriteKit will recalculate the per-pixel physics for our damaged building.
+        configurePhysics()
     }
 }
--- Project-29-Exploding-Monkeys/GameScene.swift
+++ Project-29-Exploding-Monkeys/GameScene.swift
@@ -15,7 +15,7 @@ enum CollisionTypes: UInt32 {
     case player = 4
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var buildings = [BuildingNode]()
     var player1: SKSpriteNode!
@@ -29,6 +29,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        physicsWorld.contactDelegate = self
         
         createBuildings()
         createPlayers()
@@ -40,6 +41,44 @@ class GameScene: SKScene {
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if banana != nil {
+            if banana.position.y < -1000 {
+                banana.removeFromParent()
+                banana = nil
+                
+                changePlayer()
+            }
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody: SKPhysicsBody
+        let secondBody: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if let firstNode = firstBody.node {
+            if let secondNode = secondBody.node {
+                if firstNode.name == "banana" && secondNode.name == "building" {
+                    bananaHit(building: secondNode as! BuildingNode, atPoint: contact.contactPoint)
+                    print("BUILDING HIT")
+                }
+                
+                if firstNode.name == "banana" && secondNode.name == "player1" {
+                    destroy(player: player1)
+                }
+                
+                if firstNode.name == "banana" && secondNode.name == "player2" {
+                    destroy(player: player2)
+                }
+            }
+        }
     }
     
     private func createBuildings() {
@@ -99,10 +138,11 @@ class GameScene: SKScene {
         banana.name = "banana"
         banana.physicsBody = SKPhysicsBody(circleOfRadius: banana.size.width / 2)
         banana.physicsBody?.categoryBitMask = CollisionTypes.banana.rawValue
-        banana.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue | CollisionTypes.building.rawValue
-        banana.physicsBody?.collisionBitMask = CollisionTypes.player.rawValue | CollisionTypes.building.rawValue
+        banana.physicsBody?.contactTestBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+        banana.physicsBody?.collisionBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
         banana.physicsBody?.usesPreciseCollisionDetection = true
         addChild(banana)
+        
 //        4. If player 1 was throwing the banana, we position it up and to the left of the player and give it some spin.
         if currentPlayer == 1 {
             throwBanana(player: 1, angle: radians, speed: speed)
@@ -110,10 +150,8 @@ class GameScene: SKScene {
 //        7. If player 2 was throwing the banana, we position it up and to the right, apply the opposite spin, then make it move in the correct direction.
             throwBanana(player: 2, angle: radians, speed: speed)
         }
-        
-
-        
     }
+    
     private func throwBanana(player: Int, angle: Double, speed: Double) {
         banana.position = CGPoint(x: player == 1 ? player1.position.x - 30 : player2.position.x + 30, y: player == 1 ? player1.position.y + 40 : player2.position.y + 40)
         banana.physicsBody?.angularVelocity = player == 1 ? -20 : 20
@@ -130,6 +168,48 @@ class GameScene: SKScene {
     
     private func deg2rad(degrees: Int) -> Double {
         return Double(degrees) * Double.pi / 180.0
+    }
+    
+    private func bananaHit(building: BuildingNode, atPoint contactPoint: CGPoint) {
+        let buildingLocation = convert(contactPoint, to: building)
+        building.hitAt(point: buildingLocation)
+        
+        let explosion = SKEmitterNode(fileNamed: "hitBuilding")!
+        explosion.position = contactPoint
+        addChild(explosion)
+        
+        banana.name = "" // this is to avoid the banana exploding twice when it hits two buildings by changing it's name that woll not happen and changePlayer will not be called twice little bug fix
+        banana?.removeFromParent()
+        banana = nil
+        
+        changePlayer()
+    }
+    
+    private func destroy(player: SKSpriteNode) {
+        let explosion = SKEmitterNode(fileNamed: "hitPlayer")!
+        explosion.position = player.position
+        addChild(explosion)
+        
+        player.removeFromParent()
+        banana?.removeFromParent()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            [unowned self] in
+            let newGame = GameScene(size: self.size)
+            newGame.viewController = self.viewController
+            self.viewController.currentGame = newGame
+            
+            self.changePlayer()
+            newGame.currentPlayer = self.currentPlayer
+            
+            let transition = SKTransition.doorway(withDuration: 1.5)
+            self.view?.presentScene(newGame, transition: transition)
+        }
+    }
+    
+    private func changePlayer() {
+        currentPlayer = currentPlayer == 1 ? 2 : 1
+        viewController.activatePlayer(number: currentPlayer)
     }
 
 }
